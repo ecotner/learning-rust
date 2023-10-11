67941131212fbc3a766caa2e4da3600d68e716d6
ecotner <2.71828cotner@gmail.com>
2023-10-11 08:50:31 -0700

    mars weight calculator

---
+++ mars_calc/src/main.rs
@@ -0,0 +1,23 @@
+use std::io::{self, Write};
+
+fn main() {
+    print!("Enter your weight in kg (on Earth): ");
+    io::stdout().flush().unwrap();
+    // assign variables with `let`
+    // variables are immutable by default; need to declare mutability with `mut` keyword
+    let mut input = String::new(); // create variable-size string buffer
+    // read string from standard input (stdin).
+    // the read_line function returns a Result object that could either be Ok or Err;
+    // Result.unwrap() returns the value wrapped by Ok (if it exists), otherwise it ends
+    // the program.
+    io::stdin().read_line(&mut input).unwrap();
+    // convert string to floating point value.
+    // ignore potential parsing errors for now (e.g. expecting a float but getting text).
+    let weight: f32 = input.trim().parse().unwrap();
+    let mars_weight = calculate_weight_on_mars(weight);
+    println!("Weight on Mars: {} kg", mars_weight); // the ! means this is a macro, not a function; use cargo-expand to investigate macro expansions
+}
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711 // last line of a function without a semicolon is interpreted as return
+}
